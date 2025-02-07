5b09f11f493dcc56dc0aa4e49f693e1b4d7edf08
Fance Satria Nusantara <fancesatria.rpl@gmail.com>
2025-02-07 15:03:14 +0700

    push 1

---
+++ TicTacToe/src/tictactoe/main.java
@@ -0,0 +1,15 @@
+
+package tictactoe;
+
+/**
+ *
+ * @author fance
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Play play = new Play();
+        play.show_message();
+    }
+    
+}
